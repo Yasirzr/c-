8aa3a44556772c7d9868c0b769b3f52667e239ae
Yasirzr <113897768+Yasirzr@users.noreply.github.com>
2022-11-28 14:44:17 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Yasir Zainur Rahman"<<endl;
+    cout << "Sekolah : Smkn 1 subang"<<endl;
+    cout << "Kelas : X PPLG 2"<<endl;
+    
+}
